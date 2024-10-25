9ce8875a9c5bff0204b4cae49f097a585f864593
Mohsin Hashmi <78404039+Mohsin-Hashmi@users.noreply.github.com>
2024-10-25 12:19:11 +0500

    Delete car_rental/backend directory

--- car_rental/backend/src/db/conn.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb+srv://mohsin-dev:12345@car-rental-db.4horf.mongodb.net/',{
-//     useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(()=>{
-//     console.log(`connection successfully`);
-// }).catch((err)=>{
-//     console.log(`no connection`, err);
-// });
