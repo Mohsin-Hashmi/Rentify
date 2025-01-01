7450878a3eb082a1d8e0b0092d5e2549bb2b7763
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-01-01 14:20:08 +0500

    Developed the create profile frontend page

--- car_rental/frontend/src/pages/create-profile/CreateProfile.jsx
+++ car_rental/frontend/src/pages/create-profile/CreateProfile.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "../../navbar/Navbar";
 import Footer from "../../footer/Footer";
+import "../../global/container.css";
+import "../../global/fonts.css";
+import "./CreateProfile.css";
 const CreateProfile = () => {
   return (
     <>
@@ -8,9 +11,33 @@ const CreateProfile = () => {
         <Navbar />
       </div>
 
-      <section>
+      <section className="createProfileSection">
         <div className="container">
-          <p>Hello</p>
+          <div className="createProfileWrap">
+            <h1 className="createProfileHeading">Create Your Profile</h1>
+            <form action="">
+              <div className="formGroup">
+                <label className="profileLabel" htmlFor="">First Name</label>
+                <input className="profileinputField" type="text" placeholder="Enter Your First Name" required/>
+                
+              </div>
+              <div className="formGroup">
+                <label className="profileLabel" htmlFor="">Last Name</label>
+                <input className="profileinputField" type="text" placeholder="Enter Your Last Name" required/>
+              </div>
+              <div className="formGroup">
+                <label className="profileLabel" htmlFor="">Profile Image</label>
+                <input className="profileinputField" type="file" placeholder="Insert Your Profile Image" required/>
+              </div>
+              <div className="formGroup">
+                <label className="profileLabel" htmlFor="">About</label>
+                <textarea className="profileinputField textarea"  placeholder="Enter Your Bio" required/>
+              </div>
+              <button type="submit" className="profileSubmitBtn">
+                Save Profile
+              </button>
+            </form>
+          </div>
         </div>
       </section>
       <footer className="carRentalFooter">
