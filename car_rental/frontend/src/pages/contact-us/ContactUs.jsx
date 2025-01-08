b04c19f1116b05eedc0e1a2293e97c540bd3d5be
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-01-08 19:33:11 +0500

    developing the contact-us page

--- car_rental/frontend/src/pages/contact-us/ContactUs.jsx
+++ car_rental/frontend/src/pages/contact-us/ContactUs.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "../../navbar/Navbar";
+import Footer from "../../footer/Footer";
+import "../../global/container.css";
+import "./ContactUs.css";
+import phoneLogo from '../../assets/images/phoneLogo.webp';
+import emailLogo from '../../assets/images/emailLogo.webp';
+import locationLogo from '../../assets/images/locationLogo.webp';
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <>
+      <Navbar />
+      <section className="contactUs">
+        <div className="container">
+          <div className="contactUsWrapper">
+            <h1 className="contactUsHeading">Contact Us</h1>
+            <p className="contactUsPara">
+              Any question or remarks? Just write us a message!
+            </p>
+            <div className="ContactUsBodyWrapper">
+              <div className="contactUsInfo">
+                <h2 className="contactInfoHeading">Contact Information</h2>
+                <p className="contactInfoPara">Say something to start a live chat!</p>
+                <div className="contactInfoWrapper">
+                    <div className="contactInfoWrapperContent">
+                        <img src={phoneLogo} alt="phoneLogo" />
+                        <p className="infoPara">+1012 3456 789</p>
+                    </div>
+                    <div className="contactInfoWrapperContent">
+                        <img src={emailLogo} alt="emailLogo" />
+                        <p className="infoPara">demo@gmail.com</p>
+                    </div>
+                    <div className="contactInfoWrapperContent">
+                        <img src={locationLogo} alt="locationLogo" />
+                        <p className="infoPara">
+                        132 Dartmouth Street Boston, Massachusetts 02156 United States
+                        </p>
+                    </div>
+                    
+                </div>
+              </div>
+            <div>
+                <h1>contact us form</h1>
+            </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className="carRentalFooter">
+        <div className="container">
+          <Footer />
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
